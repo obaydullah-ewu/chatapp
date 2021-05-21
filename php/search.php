37f4f4bf6443b535a3ae32b796ad4da28ceba430
Md. Obaydullah <68339585+ObaydullahEWU@users.noreply.github.com>
2021-05-21 21:13:53 +0600

    all files attached

---
+++ php/search.php
@@ -0,0 +1,39 @@
+<?php
+    session_start();
+    include_once "config.php";
+    $outgoing_id = $_SESSION['unique_id'];
+    $searchTerm = mysqli_real_escape_string($conn, $_POST['searchTerm']);
+    $output = "";
+    $sql = mysqli_query($conn, "SELECT * FROM users WHERE (NOT unique_id = {$outgoing_id}) AND (fname LIKE '%{$searchTerm}%' OR  lname LIKE  '%{$searchTerm}%')");
+    if (mysqli_num_rows($sql) > 0){
+        while ($row = mysqli_fetch_assoc($sql)) {
+            $sql2 = "SELECT * FROM messages 
+                WHERE (outgoing_msg_id = {$row['unique_id']} OR incoming_msg_id = {$row['unique_id']}) AND
+                             (outgoing_msg_id = {$outgoing_id} OR incoming_msg_id = {$outgoing_id}) ORDER BY msg_id DESC LIMIT 1";
+            $query2 = mysqli_query($conn, $sql2);
+            $row2 = mysqli_fetch_assoc($query2);
+            if (mysqli_num_rows($query2) > 0){
+                $result = $row2['msg'];
+            }else{
+                $result = "No message available";
+            }
+
+            // trimming message if word are more than 28
+            (strlen($result) > 28) ? $msg = substr($result, 0, 28) : $msg = $result;
+            ($outgoing_id == $row2['outgoing_msg_id']) ? $you = "You: " : $you = "";
+
+            $output .= '<a href="insert-chat.php?user_id='.$row['unique_id'].'">
+                    <div class="content">
+                        <img src="php/images/' . $row['img'] . '" alt="">
+                        <div class="details">
+                            <span>' . $row['fname'] . " " . $row['lname'] . '</span>
+                            <p>'.$you. $msg .'</p>
+                        </div>
+                    </div>
+                    <div class="status-dot"><i class="fas fa-circle"></i></div>
+                </a>';
+        }
+    }else{
+        $output .= "No user found related to your search term";
+    }
+    echo $output;
